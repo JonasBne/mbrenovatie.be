2ca24e315b8c0463d3444f3ddc1d853842e8e7f2
Jonas Boone <boone.jonas@outlook.be>
2023-01-11 17:55:45 +0100

    refactor: replace hard coded links with const

---
+++ src/consts/appUrls.ts
@@ -0,0 +1,15 @@
+export const appUrls = {
+    contact: {
+        base: "/contact",
+    },
+    galery: {
+        base: "/galerij",
+        carpentry: "/galerij/schrijn-en-meubelmakerij",
+        flooringAndTiles: "/galerij/vloer-en-tegelwerken",
+        outside: "/galerij/buitenwerken",
+        plasterBoardsAndPainting: "/galerij/gyproc-en-schilderwerken",
+    },
+    home: {
+        base: "/",
+    },
+};
